3a522cd7687b1eabb56f6aed7199e1080ceec178
PabitraBhandari <89492267+PabitraBhandari@users.noreply.github.com>
2021-11-15 11:28:14 -0600

    Create fighter.hpp

---
+++ Assigments/10-P03B/fighter.hpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "weapon.hpp"
+#include "helpers.hpp"
+#include "dice.hpp"
+using namespace std;
+
+#pragma once
+
+
+class BaseFighter
+{
+protected:
+    string name;
+    Weapon *weapon1;
+    int hp;
+    double rr;
+
+public:
+    BaseFighter()
+    {
+        name = "None";
+        weapon1 = new Weapon;
+    }
+
+    virtual double attack()
+    {
+        return weapon1->use();
+    }
+
+    void takeDamage(int x)
+    {
+        hp -= x;
+    }
+
+    bool alive()
+    {
+        return hp > 0;
+    }
+    int getHp()
+    {
+        return hp;
+    }
+
+    friend ostream &operator<<(ostream &os, const BaseFighter &f)
+    {
+        return os << "[" << f.name << " , " << *f.weapon1 << "]";
+    }
+};
+
+class Warrior : public BaseFighter
+{
+private:
+    Weapon *weapon2;
+
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class Wizard : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class Archer : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class Elf : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
+
+class DragonBorn : public BaseFighter
+{
+private:
+    Weapon* weapon2;
+public:
+    void attack(BaseFighter *&other)
+    {
+        int r = weapon2->use();
+        other->takeDamage(r);
+    }
+};
