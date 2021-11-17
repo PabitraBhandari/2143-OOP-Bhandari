8e901b609306d8b5b1ddce3ef377e4a875d235c4
PabitraBhandari <89492267+PabitraBhandari@users.noreply.github.com>
2021-11-17 10:31:00 -0600

    Update weapon.hpp

--- Assigments/10-P03B/weapon.hpp
+++ Assigments/10-P03B/weapon.hpp
@@ -1,45 +1,217 @@
 #include <iostream>
-#include <string>
 #include <vector>
-
+#include <string>
 #include "dice.hpp"
-#include "helpers.hpp"
 
 using namespace std;
+/**
+ * class Weapon
+ * 
+ * Description:
+ *      This class creates a base class for fist and feet weapon, or generates
+        a weapon based on random and sub classes virtually inheirits from base class.
+ * 
+ * Public Methods:
+      int useRand;
+      attackValue;
+ * 
+ * 
+ */
 
-#pragma once
+// Creates Base class for weapon and generates base value "fists and feet"
+class Weapon
+{
+    public:
+        Weapon()
+        {
+            
+            int useRand = rand()%2;             // Random dice values
 
-class Weapon {
-    string name;    // name of weapon
-    string damage;  // damage per roll or "use"
-    Dice*  die;
+            if(useRand)
+            {
+                attackValue = Dice("1d6");
+            }
+            else
+            {
+                attackValue = Dice("1d4");
+            }
+        }
+    
+    Dice attackValue;
+};
 
-public:
-    /**
-     * @brief Construct a new Base Weapon object
-     *  Also invokes the Dice class constructor with the "die" type
-     */
-    Weapon() {
-        name = "Fists&Feet";
-        damage = "1.d.4";
-        die = new Dice(damage);
-    }
-    /**
-     * @brief Overloaded Constructor for a new Base Weapon object
-     *  Also invokes the Dice class constructor with the "die" type
-     * @param choices-vector of string "die" choices like {"2.d.8","2.d.10","3.d.4"}
-     */
-    Weapon(vector< string > choices) {
-        name = "Fists&Feet";
-        damage = randomChoice(choices);
-        die = new Dice(damage);
-    }
+// Creates sword class that inheirits virtually from weapon
+class Sword : virtual public Weapon
+{
+    public:
+        Sword()
+        {
+            int useRand = rand()%3;
+            
+            if(useRand == 2)
+            {
+                attackValue = Dice("3d4");
+            }
+            else if(useRand == 1)
+            {
+                attackValue = Dice("2d6");
+            }
+            else
+            {
+                attackValue = Dice("1d12");
+            }
+        }
+    
+};
 
-    double use() {
-        return die->roll();
-    }
+// Creates Bow class that inheirits virtually from weapon
+class Bow : virtual public Weapon
+{
+    public:
+        Bow()
+        {
+            int useRand = rand()%3;
+            
+            if(useRand == 2)
+            {
+                attackValue = Dice("1d8");
+            }
+            else if(useRand == 1)
+            {
+                attackValue = Dice("2d4");
+            }
+            else
+            {
+                attackValue = Dice("1d10");
+            }
+        }
+    
+};
 
-    friend ostream& operator<<(ostream& os, const Weapon& w) {
-        return os << "[" << w.name << " , " << w.damage << "]";
-    }
+// Creates MagicSpell class that inheirits virtually from weapon
+class MagicSpell : virtual public Weapon
+{
+    public:
+        MagicSpell()
+        {
+            int useRand = rand()%4;
+            
+            if(useRand == 3)
+            {
+                attackValue = Dice("1d20");
+            }
+            else if(useRand == 2)
+            {
+                attackValue = Dice("2d10");
+            }
+            else if(useRand == 1)
+            {
+                attackValue = Dice("3d6");
+            }
+            else
+            {
+                attackValue = Dice("5d4");
+            }
+        }
+};
+
+// Creates MagicWeapon class that inheirits virtually from weapon
+class MagicWeapon : virtual public Weapon
+{
+    public:
+        MagicWeapon()
+        {
+            int useRand = rand()%2;
+
+            if(useRand)
+            {
+                attackValue = Dice("1d6");
+            }
+            else
+            {
+                attackValue = Dice("1d4");
+            }
+        }
+};
+
+// Creates FireWeapon class that inheirits virtually from weapon
+class FireWeapon : virtual public Weapon 
+{
+    public:
+        FireWeapon()
+        {
+            int useRand = rand()%2;
+
+            if(useRand)
+            {
+                attackValue = Dice("1d6");
+            }
+            else
+            {
+                attackValue = Dice("1d8");
+            }
+        }
+};
+/**
+ * class MagicSword
+ * 
+ * Description:
+ *      Creates weapon type that inheirits virtually from both weapon and weapon type
+ * 
+ */
+class MagicSword : virtual public Sword, virtual public MagicWeapon
+{
+     public:
+        MagicSword()
+        {
+            Weapon::attackValue = Sword::attackValue + MagicWeapon::attackValue;
+        }
+};
+
+/**
+ * class FireSword
+ * 
+ * Description:
+ *      Creates weapon type that inheirits virtually from both weapon and weapon type
+ * 
+ */
+class FireSword : virtual public Sword, virtual public FireWeapon
+{
+     public:
+        FireSword()
+        {
+            Weapon::attackValue = Sword::attackValue + FireWeapon::attackValue;
+        }
+};
+
+/**
+ * class MagicBow
+ * 
+ * Description:
+ *      Creates weapon type that inheirits virtually from both weapon and weapon type
+ * 
+ */
+class MagicBow : virtual public Bow, virtual public MagicWeapon
+{
+     public:
+        MagicBow()
+        {
+            Weapon::attackValue = Bow::attackValue + MagicWeapon::attackValue;
+        }
+};
+
+/**
+ * class FireBow
+ * 
+ * Description:
+ *      Creates weapon type that inheirits virtually from both weapon and weapon type
+ * 
+ */
+class FireBow : virtual public Bow, virtual public FireWeapon
+{ 
+     public:
+        FireBow()
+        {
+            Weapon::attackValue = Bow::attackValue + FireWeapon::attackValue;
+        }
 };
